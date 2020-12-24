68f3fc3a244e154d3b4135e63cd8f7dc9ce875b0
Lam Nguyen <l0nguy10@louisville.edu>
2020-12-24 09:33:06 -0500

    video 78

--- DatingApp/API/Startup.cs
+++ DatingApp/API/Startup.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Extensions;
 using API.Interfaces;
+using API.MiddleWare;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -44,11 +45,7 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            app.UseMiddleware<ExceptionMiddleware>();
             app.UseHttpsRedirection();
 
             app.UseRouting();
--- DatingApp/client-app/src/app/app.module.ts
+++ DatingApp/client-app/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
+
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +26,8 @@ import { SharedModule } from './_modules/shared.module';
     MemberListComponent,
     MemberDetailComponent,
     ListsComponent,
-    MessagesComponent
+    MessagesComponent,
+    TestErrorsComponent
   ],
   imports: [
     BrowserModule,
