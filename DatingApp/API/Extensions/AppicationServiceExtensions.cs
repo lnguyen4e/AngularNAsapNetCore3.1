ee8c64c2089150a7a8a020806347a7d5fe6463c6
Lam Nguyen <l0nguy10@louisville.edu>
2020-12-25 13:33:59 -0500

    93

--- DatingApp/API/Extensions/AppicationServiceExtensions.cs
+++ DatingApp/API/Extensions/AppicationServiceExtensions.cs
@@ -16,6 +16,7 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
             services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlServer(config.GetConnectionString("DefaultConnection"));
